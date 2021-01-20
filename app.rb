fe35e2cd9889f7d41b13ce68945175839e01797c
George Cherian <gc2920@columbia.edu>
2021-01-20 23:30:39 +0000

    temp commit

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class HangpersonApp < Sinatra::Base
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
   get '/' do
-    redirect '/new'
+    redirect '/new' 
   end
   
   get '/new' do
